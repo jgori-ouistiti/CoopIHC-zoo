74765ca645937a42d16ebb529bed1c0476be93ca
jgori <juliengori@gmail.com>
2022-03-02 17:03:20 +0100

    update

--- teaching_rl_example.py
+++ teaching_rl_example.py
@@ -14,7 +14,12 @@ def run_rl():
     param = 0.01, 0.2
     thr = 0.9
 
-    task = Task(inter_trial=inter_trial, n_item=n_item, max_iter=max_iter, is_item_specific=is_item_specific)
+    task = Task(
+        inter_trial=inter_trial,
+        n_item=n_item,
+        max_iter=max_iter,
+        is_item_specific=is_item_specific,
+    )
     user = User(param=param)
     assistant = Teacher(thr=thr)
     bundle = Bundle(task=task, user=user, assistant=assistant, random_reset=False)
@@ -32,14 +37,12 @@ def run_rl():
     print()
     print(env.observation_space)
     # Dict(turn_index:Discrete(4), round_index:Discrete(1000), position:Discrete(31), targets:MultiDiscrete([31 31 31 31 31 31 31 31]), goal:Discrete(31), user_action:Discrete(3), assistant_action:Box(1.0, 1.0, (1, 1), float32))
-    env.step({"user_action": 1})
+    env.step({"assistant_action": 1})
 
     # Use env_checker from stable_baselines3 to verify that the env adheres to the Gym API
     from stable_baselines3.common.env_checker import check_env
 
     check_env(env, warn=False)
-
-
 
 
 if __name__ == "__main__":
